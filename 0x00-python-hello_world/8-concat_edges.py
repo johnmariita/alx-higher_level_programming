97eff802f9b9880d6050e22209020f04427f5f82
johnmariita <johnmariita421@gmail.com>
2023-08-07 18:55:59 +0300

    object oriented programming

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[str.find("obj"):str.find("lang")] \
-        + str[str.find("with"):str.find("with") + 5] + str[:6]
+str = str[str.find("obj"):str.find("lang")] + "with " + str[:6]
 print(str)
