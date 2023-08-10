3135985aa89631b64ba2936bf7a7f2f4bf25bcba
johnmariita <johnmariita421@gmail.com>
2023-08-10 19:20:38 +0300

    command line arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-import sys
+from sys import argv
 
 
 def main():
-    length = len(sys.argv) - 1
+    length = len(argv) - 1
     if (length == 0):
         print(f"{length:d} arguments.")
     else:
@@ -12,7 +12,7 @@ def main():
         else:
             print(f"{length:d} arguments")
         for i in range(1, length + 1):
-            print(f"{i:d}: {sys.argv[i]}")
+            print(f"{i:d}: {argv[i]}")
 
 
 if __name__ == "__main__":
