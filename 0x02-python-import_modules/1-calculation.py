79c09e945d5da1857b3f44e8b4b67c4613eb65ee
johnmariita <johnmariita421@gmail.com>
2023-08-10 19:08:50 +0300

    calculator programme

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
-import calculator_1
-a = 10
-b = 5
-print(f"{a:d} + {b:d} = {calculator_1.add(a, b):d}")
-print(f"{a:d} - {b:d} = {calculator_1.sub(a, b):d}")
-print(f"{a:d} * {b:d} = {calculator_1.mul(a, b):d}")
-print(f"{a:d} / {b:d} = {calculator_1.div(a, b):d}")
+from calculator_1 import add, sub, mul, div
+
+
+def main():
+    a = 10
+    b = 5
+    print(f"{a:d} + {b:d} = {add(a, b):d}")
+    print(f"{a:d} - {b:d} = {sub(a, b):d}")
+    print(f"{a:d} * {b:d} = {mul(a, b):d}")
+    print(f"{a:d} / {b:d} = {div(a, b):d}")
+
+
+
+if __name__ == "__main__":
+    main()
