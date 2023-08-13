1db0a366acf13438b0f9597948812ef2e428cc08
johnmariita <johnmariita421@gmail.com>
2023-08-13 22:30:00 +0300

    print reverse

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,4 +4,5 @@ def print_matrix_integer(matrix=[[]]):
         for k in range(len(matrix[i])):
             print("{}".format(matrix[i][k]), end=" ") \
                 if k < len(matrix[i]) - 1 \
-                else print("{:d}".format(matrix[i][k]))
+                else print("{:d}".format(matrix[i][k]), end="")
+        print()
