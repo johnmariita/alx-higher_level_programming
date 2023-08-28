49d30f112bb96ddd2264521dc0ffb8206787a13b
johnmariita <johnmariita421@gmail.com>
2023-08-28 23:04:45 +0300

    safe print integer

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+        if not isinstance(value, int):
+            raise ValueError
+        print("{:d}".format(value))
+        return True
+    except ValueError:
+        return False
