00367b3ad65e4de013e1a6eb0c570ca3aaf02145
johnmariita <johnmariita421@gmail.com>
2023-08-30 05:48:22 +0300

    printing the square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""A module containing a square """
+
+
+class Square:
+    """A Square class"""
+
+    def __init__(self, size=0):
+        """
+        Initializing the class
+        Args:
+            size: the size of the square
+        Raises:
+            ValueError: less than zero size
+            TypeError: non-integer size
+        """
+
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+
+        self.__size = size
+
+    @property
+    def size(self):
+        """Gets the size of the square"""
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+        """
+        Calculates the squares area
+        Returns: returns the area of the square
+        """
+
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """printing #'s to form a square """
+
+        if self.__size == 0:
+            print()
+
+        for i in range(self.__size):
+            print("#" * self.__size)
