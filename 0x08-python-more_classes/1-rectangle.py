3543f86f9c14dbb4a6a31c7d4d6265479458ea5e
johnmariita <johnmariita421@gmail.com>
2023-09-05 16:03:23 +0300

    ValueError and TypeError during initialization

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -11,15 +11,29 @@ class Rectangle:
         Args:
             width: the width of the rectangle
             height: the height of the rectangle
+
+        Raises:
+            TypeError: when the value type is not an integer
+            ValueError: when the value is less than 0
         """
+        if width < 0:
+            raise ValueError('width must be >= 0')
+        if not isinstance(width, int):
+            raise TypeError('width must be an integer')
+        if height < 0:
+            raise ValueError('height must be >= 0')
+        if not isinstance(height, int):
+            raise TypeError('height must be an integer')
         self.__width = width
         self.__height = height
+
     @property
     def height(self):
         """
         the height of the rectangle
         """
         return self.__height
+
     @height.setter
     def height(self, value):
         """
@@ -37,12 +51,14 @@ class Rectangle:
         if value < 0:
             raise ValueError('height must be >= 0')
         self.__height = value
+
     @property
     def width(self):
         """
         the width of the rectangle
         """
         return self.__width
+
     @width.setter
     def width(self, value):
         """
