eadd58aa3fc95bed3c92129f2751f6357508b951
johnmariita <johnmariita421@gmail.com>
2024-03-27 22:37:46 +0300

    Script that creates a user on mysql server

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
 -- Script that creates a user on my MySQL server
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+ALTER USER 'user_0d_1'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd';
