00bbc472c022d435db7883f7371bc83d174b6d60
johnmariita <johnmariita421@gmail.com>
2024-03-27 22:52:39 +0300

    Script that creates a user and a database setting up the required grants to the user

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,7 @@
+-- Script that creates a database and a user
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+USE hbtn_0d_2
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* to 'user_0d_2'@'localhost';
+ALTER USER 'user_0d_2'@'localhost' IDENTIFIED WITH mysql_native_password
+	BY 'user_0d_2_pwd';
