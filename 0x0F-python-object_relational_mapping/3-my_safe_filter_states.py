babd8bc0cbbb15f8332a1fa51020ed48d3f7da01
johnmariita <johnmariita421@gmail.com>
2024-04-27 00:31:13 +0300

    safe filter

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module containing a script that lists states from a table
+starting with N"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         port=3306, password=sys.argv[2], database=sys.argv[3])
+    cu = db.cursor()
+    query = "SELECT * FROM states WHERE name='{}'".format(sys.argv[4])
+    cu.execute(query + " COLLATE utf8mb4_bin")
+    rows = cu.fetchall()
+    for row in rows:
+        print(row)
