49c1faf1d3bc08f38b4018d3517f090db0ad3f19
johnmariita <johnmariita421@gmail.com>
2024-04-26 22:37:10 +0300

    select states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module containing a script that lists states from a table"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         port=3306, password=sys.argv[2], database=sys.argv[3])
+    cu = db.cursor()
+    cu.execute("SELECT * FROM states")
+    rows = cu.fetchall()
+    for row in rows:
+        print(row)
---
+++ 0x0F-python-object_relational_mapping/README.md
@@ -0,0 +1,1 @@
+A repo on Object-Relational Mappers (ORM)
