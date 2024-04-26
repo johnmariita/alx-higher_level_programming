6003d44fa8f2a0735e1607a7c32f94bc0f2200c7
johnmariita <johnmariita421@gmail.com>
2024-04-26 23:21:26 +0300

    filter states that start with N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,8 +8,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          port=3306, password=sys.argv[2], database=sys.argv[3])
     cu = db.cursor()
-    cu.execute("SELECT * FROM states WHERE name LIKE 'N%'
-               ORDER BY id")
+    cu.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
     rows = cu.fetchall()
     for row in rows:
         print(row)
