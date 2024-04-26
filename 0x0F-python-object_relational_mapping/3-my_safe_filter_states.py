f994f6da7eaafb06107bd74d0b67f9d869db67fa
johnmariita <johnmariita421@gmail.com>
2024-04-27 00:49:51 +0300

    safe filter

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -9,11 +9,8 @@ if __name__ == "__main__":
                          port=3306, password=sys.argv[2], database=sys.argv[3])
     cu = db.cursor()
     safe_prompt = sys.argv[4].split("'")[0]
-    print(safe_prompt)
     query = "SELECT * FROM states WHERE name='{}'".format(safe_prompt)
-    print(query)
     cu.execute(query + " COLLATE utf8mb4_bin")
     rows = cu.fetchall()
     for row in rows:
         print(row)
-    cu.close()
