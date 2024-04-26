a6ac29ab3f9bba1385f9734c2addc104c513288d
johnmariita <johnmariita421@gmail.com>
2024-04-27 00:45:14 +0300

    safe filter

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,8 +8,12 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          port=3306, password=sys.argv[2], database=sys.argv[3])
     cu = db.cursor()
-    query = "SELECT * FROM states WHERE name='{}'".format(sys.argv[4])
+    safe_prompt = sys.argv[4].split("'")[0]
+    print(safe_prompt)
+    query = "SELECT * FROM states WHERE name='{}'".format(safe_prompt)
+    print(query)
     cu.execute(query + " COLLATE utf8mb4_bin")
     rows = cu.fetchall()
     for row in rows:
         print(row)
+    cu.close()
