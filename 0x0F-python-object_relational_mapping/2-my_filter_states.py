89899bd9604f77444d186b3b385628a6da0eac0e
johnmariita <johnmariita421@gmail.com>
2024-04-27 00:25:45 +0300

    get state name from user

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
                          port=3306, password=sys.argv[2], database=sys.argv[3])
     cu = db.cursor()
     query = "SELECT * FROM states WHERE name='{}'".format(sys.argv[4])
-    cu.execute(query)
+    cu.execute(query + " COLLATE utf8mb4_bin")
     rows = cu.fetchall()
     for row in rows:
         print(row)
