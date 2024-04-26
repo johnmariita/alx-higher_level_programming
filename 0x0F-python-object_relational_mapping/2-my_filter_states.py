b40ff5bc1ebcb4142e637024ff666340885b6b6f
johnmariita <johnmariita421@gmail.com>
2024-04-27 00:08:22 +0300

    get city name from user input

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module containing a script that lists states from a table
+starting with N"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         port=3306, password=sys.argv[2], database=sys.argv[3])
+    cu = db.cursor()
+    cu.execute("SELECT * FROM states WHERE name=%s", (sys.argv[4],))
+    rows = cu.fetchall()
+    for row in rows:
+        print(row)
