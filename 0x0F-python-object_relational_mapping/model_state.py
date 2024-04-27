6e04406a79145ef935b25533c024656591978eaa
johnmariita <johnmariita421@gmail.com>
2024-04-27 23:37:08 +0300

    definition of the state class

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""A script that defines the class state
+used to create table states"""
+
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Definition of the class state"""
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
