5a3332b1533e632711372a3b5e70d244b1f97173
johnmariita <johnmariita421@gmail.com>
2024-04-28 08:47:56 +0300

    new state

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -14,3 +14,6 @@ class State(Base):
 
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
+    
+    def __init__(self, name):
+        self.name = name
