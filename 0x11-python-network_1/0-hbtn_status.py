5091fd6ea03e3ea496cbe23fd69ea7538fe3ea27
johnmariita <johnmariita421@gmail.com>
2024-05-12 14:52:08 +0300

    GET request

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,6 +5,6 @@ import urllib.request
 with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
     body = response.read()
     print("Body Response")
-    print(f"    -type: {type(body)}")
-    print(f"    -content: {body}")
-    print(f"    -utf8 content: {body.decode('utf-8')}")
+    print(f"    - type: {type(body)}")
+    print(f"    - content: {body}")
+    print(f"    - utf8 content: {body.decode('utf-8')}")
