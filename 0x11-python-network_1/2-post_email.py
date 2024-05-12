9b3a0c5716e5f0c4b760434a02ef35947da515ea
johnmariita <johnmariita421@gmail.com>
2024-05-12 23:14:21 +0300

    post request

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""A script that sends a post request"""
+
+import sys
+import urllib.request
+import urllib.parse
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    data = {'email': 'hr@holbertonschool.com'}
+    en_data = urllib.parse.urlencode(data)
+    en_data = en_data.encode('ascii')
+    with urllib.request.urlopen(url, en_data) as response:
+        print(response.read().decode('utf8'))
