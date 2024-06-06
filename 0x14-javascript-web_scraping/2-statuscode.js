82bf0330d24991e81d9dd8259872a39d0da8b37a
johnmariita <johnmariita421@gmail.com>
2024-06-06 04:59:34 +0300

    status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const request = require('request');
+const { argv } = require('node:process');
+
+request(argv[2], function (error, response, body) {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  console.log('Code: ', response.statusCode);
+});
